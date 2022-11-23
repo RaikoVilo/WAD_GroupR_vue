065c6623c552102a5cb27581348423d120b6fdf7
reesiklm <you@example.com>
2022-11-23 20:49:56 +0200

    Turned it into singup page

--- group_r/src/views/LoginPage.vue
+++ group_r/src/views/LoginPage.vue
@@ -4,30 +4,46 @@
           <div class="login_welcome">
               <label>Welcome to PostIt</label>
           </div>
-          <div class="login_blue_text">
+          <div class="login_black_text">
               <label>Create an account</label>
           </div>
-          <div class="login_black_text">
-              <label>or<br>
-              Please log in</label>
-          </div>
+
           <div class="login_input_text">
               <input class="input_class" type="Email" id="email" placeholder="Email" name="email" required><br>
               <input class="input_class" type="Password" id="password" placeholder="Password" name="password" required><br>
               <a id="submit" >
-                <input @click="() => checkPassword()"  type="submit" id="submit" value="Log in"/>
+                <input @click="() => checkPassword()"  type="submit" id="submit" value="Signup"/>
               </a>
 
           </div>
           <div class="log_in_button">
               
           </div>
-          <div class="login_blue_text">
-              <label>Forget password</label>
-          </div>
+
       </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: "password",
+  methods: {
+    checkPassword() {
+      
+      if(document.getElementById("password").value.match(/(?=.*\d)(?=(.*[a-z]){2,})(?=.*[A-Z])(?=.*[_])(?=.*[0-9]).{8,15}/g) && document.getElementById("password").value.length < 15 ) {
+        window.location="index.html";
+      }
+      else {
+        alert("This password is not valid \n The password needs to: \n * Be at least 8 and less than 15 chars \n * Include at least one uppercase alphabet character \n * Include at least two lowercase alphabet characters \n * Include at least one numeric value \n * Start with an uppercase alphabet \n * Include the character '_' ");
+      }
+    }
+  }
+}
+  
+
+</script>
+
 
 <style scoped>
 
@@ -86,24 +102,5 @@
   }
 </style>
 
-<script>
-
-export default {
-  name: "password",
-  methods: {
-    checkPassword() {
-      
-      if(document.getElementById("password").value.match(/(?=.*\d)(?=(.*[a-z]){2,})(?=.*[A-Z])(?=.*[_])(?=.*[0-9]).{8,15}/g) && document.getElementById("password").value.length < 15 ) {
-        window.location="index.html";
-      }
-      else {
-        alert("This password is not valid \n The password needs to: \n * Be at least 8 and less than 15 chars \n * Include at least one uppercase alphabet character \n * Include at least two lowercase alphabet characters \n * Include at least one numeric value \n * Start with an uppercase alphabet \n * Include the character '_' ");
-      }
-    }
-  }
-}
-  
-
-</script>
 
 
